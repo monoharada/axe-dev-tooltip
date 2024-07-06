d83ef17504093cd3b0c7dfa211100014f5fd9fc2
monoharada <harada@monosus.co.jp>
2024-07-07 02:44:44 +0900

    chore fix package

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { InsertScriptPlugin } from "./plugins/InsertScriptPlugin";
-
-
-
-
-export default defineConfig({
-  plugins: [InsertScriptPlugin('ja')],
-});
