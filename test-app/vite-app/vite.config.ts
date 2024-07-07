542dc6c48dec0113a419df7bb218e41bfe41f9c4
monoharada <harada@monosus.co.jp>
2024-07-07 18:32:54 +0900

    update

--- test-app/vite-app/vite.config.ts
+++ test-app/vite-app/vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),InsertAxeScriptVite('ja')],
+  plugins: [react(),InsertAxeScriptVite({locale:'ja'})],
 })
